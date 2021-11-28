575c015bc8c8758739eeba65d7349d5a02e960a4
Sergey Tyurikov <project-present@yandex.ru>
2021-11-29 04:35:31 +0700

    '5'

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Header } form "./Header";
+
+window.addEventListener('load', listener: () => {
+    ReactDom.render(<Header/>, document.getElementById(elementid: 'react_root'));
+});
